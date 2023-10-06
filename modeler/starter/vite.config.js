ff3c7950b5f64cac9d857ba9e85ea523275b93b0
Hugo Andres Lopez Acosta <hulo@dtu.dk>
2023-10-06 16:03:43 +0200

    added changes according to DCR-js repo

--- modeler/starter/vite.config.js
+++ modeler/starter/vite.config.js
@@ -1,10 +1,13 @@
 import { defineConfig } from 'vite'
 
+ 
+
 export default defineConfig({
   root: 'src',
   assetsInclude: ['**/*.xml'],
   server: {
     hmr: false,
   },
-  resolve: { alias: { stream: "stream-browserify" } }
+  resolve: { alias: { stream: "stream-browserify" } },
+  base: './'
 })
